aed8d3d6d070a04eaf3e12ddb19fb2043ca7f0de
Kamil <kamil@boostorder.com>
2019-10-06 23:46:55 +0800

    - templateSelector added

---
+++ EzyhaulAssessment.Forms.UI/Templates/DeliveryLocation.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EzyhaulAssessment.Forms.UI.Templates
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DeliveryLocation : Grid
+    {
+        public DeliveryLocation()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ EzyhaulAssessment.Forms.UI/Templates/LocationTemplateSelector.cs
@@ -0,0 +1,30 @@
+﻿using EzyhaulAssessment.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace EzyhaulAssessment.Forms.UI.Templates
+{
+    public class LocationTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate DeliveryLocationTemplate = new DataTemplate(typeof(DeliveryLocation));
+
+        public DataTemplate PickupLocationTemplate = new DataTemplate(typeof(PickupLocation));
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var temp = item as Location;
+
+            if(temp.type == "DEL")
+            {
+                return DeliveryLocationTemplate;
+            }
+            else
+            {
+                return PickupLocationTemplate;
+            }
+
+        }
+    }
+}
